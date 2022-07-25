4a4ad2d05bcd24fec55d7e40c353c41344cb3431
pardparw <87960461+pardparw@users.noreply.github.com>
2022-07-25 20:17:38 +0700

    Add files via upload

---
+++ index/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <title>Home</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Home.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 4.14.1, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    
+    
+
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Home">
+    <meta property="og:type" content="website">
+  </head>
+  <body data-home-page="Home.html" data-home-page-title="Home" class="u-body u-xl-mode" data-lang="en"><header class="u-clearfix u-header u-header" id="sec-6624"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="index.php" class="u-image u-logo u-image-1" data-image-width="128" data-image-height="128">
+          <img src="https://i.imgur.com/0GkF17z.jpg" class="u-logo-image u-logo-image-1">
+        </a>
+        <nav class="u-menu u-menu-one-level u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="index.html" style="padding: 10px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="https://discord.com/api/oauth2/authorize?client_id=865808924472377374&amp;permissions=1644971949303&amp;scope=bot%20applications.commands" style="padding: 10px 20px;">invite</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="wiki.html" target="_blank" style="padding: 10px 20px;">wiki</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="https://discord.gg/WsDtjhBjCV" target="_blank" style="padding: 10px 20px;">Server</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="index.html">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="https://discord.com/api/oauth2/authorize?client_id=865808924472377374&amp;permissions=1644971949303&amp;scope=bot%20applications.commands">invite</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="wiki.php" target="_blank">wiki</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="https://discord.gg/WsDtjhBjCV" target="_blank">Server</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></header>
+    <section class="u-align-left u-clearfix u-image u-shading u-section-1" id="carousel_ba85" width="1000" height="420" img src="https://media.discordapp.net/attachments/902472433062273046/1001110197735018526/2089782.png" alt="">
+      <div class="u-clearfix u-sheet u-valign-middle-xs u-sheet-1">
+        <h3 class="u-text u-text-1">IDK name bot</h3>
+        <h5 class="u-text u-text-2">Invites</h5>
+        <div class="u-list u-list-1">
+          <div class="u-repeater u-repeater-1">
+            <div class="u-align-center u-container-style u-list-item u-repeater-item u-white">
+              <div class="u-container-layout u-similar-container u-container-layout-1"><span class="u-file-icon u-icon u-icon-1" data-href="https://discord.com/api/oauth2/authorize?client_id=865808924472377374&amp;permissions=1644971949303&amp;scope=bot%20applications.commands" data-target="_blank"><img src="https://media.discordapp.net/attachments/902472433062273046/1001110197735018526/2089782.png" alt=""></span>
+                <h5 class="u-text u-text-3">Invites</h5>
+              </div>
+            </div>
+            <div class="u-align-center u-container-style u-list-item u-repeater-item u-white u-list-item-2">
+              <div class="u-container-layout u-similar-container u-container-layout-2"><span class="u-file-icon u-icon u-icon-2" data-href="https://discord.gg/WsDtjhBjCV" data-target="_blank"><img src="https://cdn.discordapp.com/attachments/902472433062273046/1001110575612444712/Discord-Logo-Square-768x768.png" alt=""></span>
+                <h5 class="u-text u-text-4">Bot server</h5>
+              </div>
+            </div>
+            <div class="u-align-center u-container-style u-list-item u-repeater-item u-white">
+              <div class="u-container-layout u-similar-container u-container-layout-3"><span class="u-file-icon u-icon u-icon-3" data-href="wiki.html"><img src="https://cdn.discordapp.com/attachments/902472433062273046/1001110810258571324/Wikipedia-logo-v2.svg.png" alt=""></span>
+                <h5 class="u-text u-text-5">Wiki</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+
+  
+</body></html>

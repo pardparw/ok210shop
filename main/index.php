8c62dbfedcc804e00cc705ac61177556ed2d3d0b
pardparw <87960461+pardparw@users.noreply.github.com>
2022-09-09 09:27:49 +0700

    Update index.php

--- main/index.php
+++ main/index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../assets/images/nyan-cat.gif">
+    <link rel="icon" href="favicon.ico">
       <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
     <title>Pardparw Website | Home page</title>
